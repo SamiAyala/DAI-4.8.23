17bfa0e951f91964d166c46db489440641502286
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-08 12:10:53 -0300

    Quiza falta 1 funcion

--- Back/Services.js
+++ Back/Services.js
@@ -20,13 +20,49 @@ export class Usuario {
     }
 
     static Register = async (Usuario) => {
-        const { nombre, telefono, mail, contrasenia, } = Usuario
+        const { nombre, contrasenia, } = Usuario
         let pool = await sql.connect(config)
         let result = await pool.request()
-            .input('Nombre', sql.NVarChar(4000), nombre)
-            .input('Telefono', sql.NVarChar(4000), telefono)
-            .input('Mail', sql.NVarChar(4000), mail)
-            .input('Contrasenia', sql.NVarChar(4000), contrasenia)
-            .query('INSERT INTO Usuario (Nombre, Telefono, Mail, Contrasenia) VALUES (@Nombre, @Telefono, @Mail, @Contrasenia)')
+            .input('pNombre', sql.NVarChar(4000), nombre)
+            .input('pContrasenia', sql.NVarChar(4000), contrasenia)
+            .query('INSERT INTO Usuario (Nombre, Contrasenia) VALUES (@pNombre, @pContrasenia)')
     }
+}
+
+
+export class Perfil{
+    
+    static Register = async (Perfil) =>{
+        const {nombreUsuario, apellido, telefono, mail, fechaNacimiento} = Perfil
+        let pool = await sql.connect(config)
+        let result = await pool.request()
+        .input('pNombreUsuario',sql.NVarChar(4000),nombreUsuario)
+        .input('pApellido',sql.NVarChar(4000),apellido)
+        .input('pTelefono',sql.NVarChar(4000),telefono)
+        .input('pMail',sql.NVarChar(4000),mail)
+        .input('pFechaNacimiento',sql.Date,fechaNacimiento)
+        .query("INSERT INTO Perfil(NombreUsuario,Apellido,Telefono,Mail,FechaNacimiento) VALUES (@pNombreUsuario, @pApellido, @pTelefono, @pMail, @pFechaNacimiento)")
+        
+    }
+
+    static Update = async (Perfil) => {
+        const { Id, nombreUsuario, apellido, telefono, mail, fechaNacimiento} = Perfil
+        let returnEntity = null;
+        console.log("Estoy en: update");
+        try {
+            let pool = await sql.connect(config)
+            let result = await pool.request()
+                .input('pNombreUsuario',sql.NVarChar(4000),nombreUsuario)
+                .input('pApellido',sql.NVarChar(4000),apellido)
+                .input('pTelefono',sql.NVarChar(4000),telefono)
+                .input('pMail',sql.NVarChar(4000),mail)
+                .input('pFechaNacimiento',sql.Date,fechaNacimiento)
+                .query('UPDATE Perfil SET NombreUsuario = @pNombreUsuario, Apellido = @pApellido, Telefono = @pTelefono, Mail = @pMail, FechaNacimiento = @pFechaNacimiento,   WHERE Perfil.Id = @pId')
+            returnEntity = result.recordsets[0];
+        } catch (error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+
 }
